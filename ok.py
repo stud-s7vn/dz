0d575e85f3806bd69e345094e0fdd2c6b0a3971d
stud-s7vn <vsevolod.loginov2021@gmail.com>
2026-05-02 15:34:38 +0300

    dz 1
    first homework

---
+++ ok.py
@@ -0,0 +1,32 @@
+#ok so hi hello hi
+class bank_acount:
+    def __init__(self, money):
+        self.money = money
+    def withdraw(self):
+        cassh = int(input("how much money to withdraw? "))
+        if cassh > self.money:
+            print ("not enough green in bank!")
+        elif cassh == 0:
+            print ("are we serious right now")
+        else:
+            self.money -= cassh
+
+            print("money on bank acount: " + str(self.money))
+    def deposit(self):
+        self.money += int(input("how much money to deposit? "))
+        print("money on bank acount: " + str(self.money))
+
+    def command(self):
+        comand = str(input("what action do you want to do(withdraw or deposit)? "))
+        if comand == "withdraw":
+            self.withdraw()
+        elif comand == "deposit":
+            self.deposit()
+        else:
+            print("incorrect action")
+
+cash = bank_acount(int(input("how much starting money? ")))
+
+for i in range(100):
+    cash.command()
+#purely human no ai
